248edefb3d9f1b98322f549fe301d5f0fdbcc9cd
John Stange <stange@johnstange.net>
2017-03-16 10:14:53 -0400

    grammatical and syntactical twiddles

--- source/chrome/actionstations.js
+++ source/chrome/actionstations.js
@@ -26,12 +26,13 @@ $(document).ready(function() {
   chrome.storage.onChanged.addListener(function(changes, namespace) {
     for (key in changes) {
       var storageChange = changes[key];
-//      console.log('Storage key "%s" in namespace "%s" changed. ' +
+/*      console.log('Storage key "%s" in namespace "%s" changed. ' +
                   'Old value was "%s", new value is "%s".',
                   key,
                   namespace,
                   storageChange.oldValue,
                   JSON.stringify(storageChange.newValue, null, " "));
+ */
     }
   });
 
